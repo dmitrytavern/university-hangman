43e343334ad47ab791f9be7fe21a1bbd6b97f94d
dmitrytavern <dmitrytavern@users.noreply.github.com>
2023-05-04 17:34:28 +0000

    deploy: 9dbef1ef0cdbc9b6474443377ab2f83973a3ad46

---
+++ assets/guide_iterations_index.md.cecd7726.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,O as i}from"./chunks/framework.389d77d5.js";const u=JSON.parse('{"title":"Спринти проекту","description":"","frontmatter":{},"headers":[],"relativePath":"guide/iterations/index.md"}'),r={name:"guide/iterations/index.md"},n=i("",4),o=[n];function s(_,p,l,c,d,h){return a(),t("div",null,o)}const f=e(r,[["render",s]]);export{u as __pageData,f as default};
