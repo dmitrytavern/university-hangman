4b6db62481355a7ced5efc80b9d4defd42b5f701
Kyrylo Muzychka <kyryll2016@gmail.com>
2023-05-28 14:01:21 +0300

    refactor: gamestartwidget

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -7,54 +7,64 @@
 
 MainWindow::MainWindow() : QWidget()
 {
-  HomeWidget *homeWidget = new HomeWidget();
-  GameWidget *gameWidget = new GameWidget();
-  AboutWidget *aboutWidget = new AboutWidget();
-  GameOverWidget *gameOverWidget = new GameOverWidget();
-  GameStartWidget *gameStartWidget = new GameStartWidget();
+    HomeWidget *homeWidget = new HomeWidget();
+    GameWidget *gameWidget = new GameWidget();
+    AboutWidget *aboutWidget = new AboutWidget();
+    GameOverWidget *gameOverWidget = new GameOverWidget();
+    GameStartWidget *gameStartWidget = new GameStartWidget();
 
-  QStackedWidget *stackedWidget = new QStackedWidget();
-  stackedWidget->addWidget(homeWidget);
-  stackedWidget->addWidget(aboutWidget);
-  stackedWidget->addWidget(gameWidget);
-  stackedWidget->addWidget(gameOverWidget);
-  stackedWidget->addWidget(gameStartWidget);
-  stackedWidget->setCurrentWidget(homeWidget);
+    //
+    const QPalette  homePalette(QColor(5,134,96));
+    homeWidget->setPalette(homePalette);
+    homeWidget->setAutoFillBackground(true);
 
-  QHBoxLayout *layout = new QHBoxLayout(this);
-  layout->addWidget(stackedWidget);
+    const QPalette  gameStartPalette(QColor(120,94,26));
+    gameStartWidget->setPalette(gameStartPalette);
+    gameStartWidget->setAutoFillBackground(true);
+    //
 
-  QObject::connect(homeWidget, &HomeWidget::GoToStartGame, [=, this]() {
-    stackedWidget->setCurrentWidget(gameStartWidget);
-  });
-
-  QObject::connect(homeWidget, &HomeWidget::GoToAbout, [=, this]() {
-    stackedWidget->setCurrentWidget(aboutWidget);
-  });
-
-  QObject::connect(aboutWidget, &AboutWidget::GoToHome, [=, this]() {
+    QStackedWidget *stackedWidget = new QStackedWidget();
+    stackedWidget->addWidget(homeWidget);
+    stackedWidget->addWidget(aboutWidget);
+    stackedWidget->addWidget(gameWidget);
+    stackedWidget->addWidget(gameOverWidget);
+    stackedWidget->addWidget(gameStartWidget);
     stackedWidget->setCurrentWidget(homeWidget);
-  });
 
-  QObject::connect(gameStartWidget, &GameStartWidget::GoToHome, [=, this]() {
-    stackedWidget->setCurrentWidget(homeWidget);
-  });
+    QHBoxLayout *layout = new QHBoxLayout(this);
+    layout->addWidget(stackedWidget);
 
-  QObject::connect(gameStartWidget, &GameStartWidget::GoToGame, [=, this]() {
-    stackedWidget->setCurrentWidget(gameWidget);
-  });
+    QObject::connect(homeWidget, &HomeWidget::GoToStartGame, [=, this]() {
+        stackedWidget->setCurrentWidget(gameStartWidget);
+    });
 
-  QObject::connect(gameWidget, &GameWidget::GoToGameOver, [=, this]() {
-    stackedWidget->setCurrentWidget(gameOverWidget);
-  });
+    QObject::connect(homeWidget, &HomeWidget::GoToAbout, [=, this]() {
+        stackedWidget->setCurrentWidget(aboutWidget);
+    });
 
-  QObject::connect(gameOverWidget, &GameOverWidget::GoToGame, [=, this]() {
-    stackedWidget->setCurrentWidget(gameWidget);
-  });
+    QObject::connect(aboutWidget, &AboutWidget::GoToHome, [=, this]() {
+        stackedWidget->setCurrentWidget(homeWidget);
+    });
 
-  QObject::connect(gameOverWidget, &GameOverWidget::GoToHome, [=, this]() {
-    stackedWidget->setCurrentWidget(homeWidget);
-  });
+    QObject::connect(gameStartWidget, &GameStartWidget::GoToHome, [=, this]() {
+        stackedWidget->setCurrentWidget(homeWidget);
+    });
 
-  QObject::connect(homeWidget, &HomeWidget::GoToExit, [=, this]() { exit(0); });
+    QObject::connect(gameStartWidget, &GameStartWidget::GoToGame, [=, this]() {
+        stackedWidget->setCurrentWidget(gameWidget);
+    });
+
+    QObject::connect(gameWidget, &GameWidget::GoToGameOver, [=, this]() {
+        stackedWidget->setCurrentWidget(gameOverWidget);
+    });
+
+    QObject::connect(gameOverWidget, &GameOverWidget::GoToGame, [=, this]() {
+        stackedWidget->setCurrentWidget(gameWidget);
+    });
+
+    QObject::connect(gameOverWidget, &GameOverWidget::GoToHome, [=, this]() {
+        stackedWidget->setCurrentWidget(homeWidget);
+    });
+
+    QObject::connect(homeWidget, &HomeWidget::GoToExit, [=, this]() { exit(0); });
 }
--- src/widgets/gamestartwidget/gamestartwidget.cpp
+++ src/widgets/gamestartwidget/gamestartwidget.cpp
@@ -1,20 +1,47 @@
 #include "gamestartwidget.h"
+#include <QLineEdit>
+#include <QPalette>
+
 
 GameStartWidget::GameStartWidget() : QWidget()
 {
-  QLabel *label = new QLabel("Game Start Widget");
-  QPushButton *gameButton = new QPushButton("Go to Game");
-  QPushButton *homeButton = new QPushButton("Go to Home");
+    QVBoxLayout *layout = new QVBoxLayout(this);
 
-  QVBoxLayout *layout = new QVBoxLayout(this);
-  layout->addWidget(label);
-  layout->addWidget(gameButton);
-  layout->addWidget(homeButton);
-  layout->setAlignment(Qt::AlignTop);
+    //
+    QLabel *label = new QLabel("Game Start Widget");
+    layout->addWidget(label);
+    //
 
-  connect(homeButton, &QPushButton::clicked, this,
-          [=, this]() { emit GoToHome(); });
+    QVBoxLayout *layoutTitle = new QVBoxLayout(this);
+    QLabel *labelTitle = new QLabel("Hangman");
+    layoutTitle->addWidget(labelTitle);
 
-  connect(gameButton, &QPushButton::clicked, this,
-          [=, this]() { emit GoToGame(); });
+    QVBoxLayout *layoutRequest = new QVBoxLayout(this);
+    QLabel *labelRequest = new QLabel("Enter names of players");
+    layoutRequest->addWidget(labelRequest);
+
+    QHBoxLayout *layoutEdits = new QHBoxLayout(this);
+    QLineEdit* editNameOfPlayer1 = new QLineEdit("USERNAME1");
+    QLineEdit* editNameOfPlayer2 = new QLineEdit("USERNAME2");
+    layoutEdits->addWidget(editNameOfPlayer1);
+    layoutEdits->addWidget(editNameOfPlayer2);
+
+    QHBoxLayout *layoutButtons = new QHBoxLayout(this);
+    QPushButton *gameButton = new QPushButton("Start (Go to Game)");
+    QPushButton *homeButton = new QPushButton("Back (Go to Home)");
+    layoutButtons->addWidget(homeButton);
+    layoutButtons->addWidget(gameButton);
+
+    layout->addLayout(layoutTitle);
+    layout->addLayout(layoutRequest);
+    layout->addLayout(layoutEdits);
+    layout->addLayout(layoutButtons);
+    layout->setAlignment(Qt::AlignTop);
+
+
+    connect(homeButton, &QPushButton::clicked, this,
+            [=, this]() { emit GoToHome(); });
+
+    connect(gameButton, &QPushButton::clicked, this,
+            [=, this]() { emit GoToGame(); });
 }
