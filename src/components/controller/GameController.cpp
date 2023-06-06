a90601ac2cb970bbdc11115d6c5e9929ff64ee5c
Kyrylo Muzychka <kyryll2016@gmail.com>
2023-06-06 17:12:15 +0300

    feat: add CheckMove and ClickLetter in GameController

--- src/components/controller/GameController.cpp
+++ src/components/controller/GameController.cpp
@@ -4,6 +4,31 @@
 
 GameController::GameController() {}
 
-void GameController::ClickLetter(QString letter) { qDebug() << letter; }
+void GameController::CheckMove(QString letter, QString word,
+                               std::vector<QString> *arrayOfCorectness,
+                               std::vector<QString> *arrayOfErrors) {
+  for (int i = 0; i < word.size(); i++) {
+    if (letter == word[i]) {
+      arrayOfCorectness->push_back(letter);
+      return;
+    }
+  }
+  arrayOfErrors->push_back(letter);
+}
+
+void GameController::ClickLetter(QString letter) {
+  if (currentPlayer == 1) {
+    CheckMove(letter, wordForPLayer1, &arrayOfCorectnessFor1,
+              &arrayOfErrorsFor1);
+  } else {
+    CheckMove(letter, wordForPLayer2, &arrayOfCorectnessFor2,
+              &arrayOfErrorsFor2);
+  }
+  //
+  qDebug() << letter;
+  qDebug() << arrayOfCorectnessFor1;
+  qDebug() << arrayOfErrorsFor1;
+  //
+}
 
 void GameController::Next() { currentPlayer = currentPlayer == 1 ? 2 : 1; }
--- src/components/controller/gamecontroller.h
+++ src/components/controller/gamecontroller.h
@@ -2,18 +2,23 @@
 #define GameController_H
 
 #include <QString>
-
+#include <vector>
 class GameController
 {
 public:
     GameController();
     static inline QString nameOfPlayer1;
     static inline QString nameOfPlayer2;
-    static inline QString wordForPLayer1;
-    static inline QString wordForPLayer2;
+    static inline QString wordForPLayer1 = "QWERT";
+    static inline QString wordForPLayer2 = "ASDFG";
+    static inline std::vector<QString> arrayOfErrorsFor1;
+    static inline std::vector<QString> arrayOfErrorsFor2;
+    static inline std::vector<QString> arrayOfCorectnessFor1;
+    static inline std::vector<QString> arrayOfCorectnessFor2;
     static inline int currentPlayer = 1;
-
-//signals:
+    static void CheckMove(QString letter, QString word,
+                          std::vector<QString> *arrayOfCorectness,
+                          std::vector<QString> *arrayOfErrors);
 public slots:
     static void ClickLetter(QString letter);
     static void Next();
